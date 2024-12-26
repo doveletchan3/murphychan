eb0997a9534490772a5a66dd121cf632d8b18482
doveletchan3 <doveletchan3@gmail.com>
2024-12-26 00:15:43 -0800

    1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const episodes = {
-
-  dramaE: ["16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
   dramaG: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3"],
 };
-
+  dramaH: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3" ],
+};
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaE: 16, // Start displaying from episode 16
   dramaG: 1, // Start displaying from episode 1
+  dramaH: 1, // Start displaying from episode 1-20
 };
 
 const audioPlayer = document.getElementById("audio-player");
