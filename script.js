d7376f324456543dbdaa838c303f9302088264b6
doveletchan3 <doveletchan3@gmail.com>
2024-12-29 15:36:36 -0800

    笑傲江湖 (1-30)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const episodes = {
-  dramaF: ["01.mp3", "03.mp3", "05.mp3", "07.mp3", "09.mp3", "11.mp3", "13.mp3", "15.mp3", "17.mp3", "19.mp3"],
+  dramaF: ["02.mp3", "04.mp3", "06.mp3", "08.mp3", "10.mp3", "12.mp3", "14.mp3", "16.mp3", "18.mp3", "20.mp3", "22.mp3", "24.mp3", "26.mp3", "28.mp3", "30.mp3"],
   dramaK: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3"],  
   dramaH: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   dramaL: ["01.mp3", "02.mp3", "03.mp3", "04.mp3"], 
@@ -8,7 +8,7 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaF: 1, // Start displaying from episode 1
+  dramaF: 2, // Start displaying from episode 2
   dramaK: 1, // Start displaying from episode 1
   dramaH: 1, // Start displaying from episode 1
   dramaL: 1, // Start displaying from episode 1
