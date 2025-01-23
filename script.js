8ed1b75d399fa3140e51043523d690eb8363c076
doveletchan3 <doveletchan3@gmail.com>
2025-01-23 00:15:00 -0800

    神鵰俠侶 (11-20)

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 const episodes = {
-  dramaR: ["46.mp3"],
-  dramaS: ["76-78.mp3"],
-  dramaR2: ["56-59.mp3"],
-  dramaP: ["81.mp3"],
-  dramaP2: ["84.mp3"],
-  dramaS2: ["61-63.mp3", "64-66.mp3"],
-  dramaQ: ["82-83.mp3"],
-  dramaQ2: ["85-86.mp3", "87-88.mp3", "89-90.mp3", "91-92.mp3"],
-  dramaQ3: ["79-80.mp3"],
-  dramaT: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
+  dramaT: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   Song1: ["song1.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaR: 46, // Start displaying from episode 46
-  dramaS: 76, // Start displaying from episode 76
-  dramaR2: 56, // Start displaying from episode 56
-  dramaP: 81, // Start displaying from episode 81
-  dramaP2: 84, // Start displaying from episode 84
-  dramaS2: 61, // Start displaying from episode 61
-  dramaQ: 82, // Start displaying from episode 82
-  dramaQ2: 85, // Start displaying from episode 85
-  dramaQ3: 79, // Start displaying from episode 79
   dramaT: 1, // Start displaying from episode 1
   Song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaP: 1, // Increment by 1
-  dramaP2: 1, // Increment by 1
-  dramaQ: 2, // Increment by 2
-  dramaQ2: 2, // Increment by 2
-  dramaQ3: 2, // Increment by 2
-  dramaR: 4, // Increment by 4
-  dramaS: 3, // Increment by 3
-  dramaR2: 4, // Increment by 4
-  dramaS2: 3, // Increment by 3
   dramaT: 1, // Increment by 1
   Song1: 1, // Increment by 1
 };
