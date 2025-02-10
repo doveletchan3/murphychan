57140fbca0df14e813d80ae5a4c94ec17bf7dc49
doveletchan3 <doveletchan3@gmail.com>
2025-02-10 00:02:54 -0800

    雍正皇帝 30-45

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const episodes = {
-  dramaB: ["16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3"],
+  dramaB: ["30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaB: 16, // Start displaying from episode 16
+  dramaB: 30, // Start displaying from episode 30
   song1: 1, // Start displaying from episode 1
 };
 
