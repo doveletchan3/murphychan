8c689e5bc0ce7c79bef139a2f6edb96fc3e41959
doveletchan3 <doveletchan3@gmail.com>
2025-02-13 00:10:55 -0800

    雍正皇帝 (53-68)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const episodes = {
-  dramaB: ["44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3", "49.mp3", "50.mp3", "51.mp3", "52.mp3", "53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3"],
+  dramaB: ["53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3", "61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaB: 44, // Start displaying from episode 44
+  dramaB: 52, // Start displaying from episode 52
   song1: 1, // Start displaying from episode 1
 };
 
