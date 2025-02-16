4d27aff228793285867e028fee6357844181594d
doveletchan3 <doveletchan3@gmail.com>
2025-02-15 23:49:28 -0800

    賴布衣傳奇 (8-23)

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const episodes = {
-  dramaB: ["61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3","71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3" ],
-  dramaC: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3","11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3","21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3"],
+  dramaC: ["08.mp3", "09.mp3", "10.mp3","11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3","21.mp3", "22.mp3", "23.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaB: 61, // Start displaying from episode 61
-  dramaC: 1, // Start displaying from episode 1
+  dramaC: 8, // Start displaying from episode 8
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaB: 1, // Increment by 1
   dramaC: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
