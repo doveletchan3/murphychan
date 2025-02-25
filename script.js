065d696dc03ae3357ac96fa85bd4ec9ff1efca5b
doveletchan3 <doveletchan3@gmail.com>
2025-02-25 00:23:12 -0800

    新編嶺南掌故 (21-30)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const episodes = {
-  dramaF: ["12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
-  dramaG: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
+  dramaF: ["21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3"],
+  dramaG: ["03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaF: 12, // Start displaying from episode 12
-  dramaG: 1, // Start displaying from episode 1
+  dramaF: 21, // Start displaying from episode 21
+  dramaG: 3, // Start displaying from episode 3
   song1: 1, // Start displaying from episode 1
 };
 
