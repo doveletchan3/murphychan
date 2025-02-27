d66b52d6ac57e06e579ae2368b938086f1f9cb1a
doveletchan3 <doveletchan3@gmail.com>
2025-02-27 00:15:03 -0800

    新編嶺南掌故 (21-44)

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const episodes = {
-  dramaF: ["21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3"],
-  dramaG: ["06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
+  dramaF: ["21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
   dramaF: 21, // Start displaying from episode 21
-  dramaG: 6, // Start displaying from episode 6
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
   dramaF: 1, // Increment by 1
-  dramaG: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
