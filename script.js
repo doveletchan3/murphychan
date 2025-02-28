5af4026fc8704da17851389bd1c696942d405513
doveletchan3 <doveletchan3@gmail.com>
2025-02-28 00:01:57 -0800

    新編嶺南掌故 (30-53)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const episodes = {
-  dramaF: ["21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3"],
+  dramaF: ["30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3", "49.mp3", "50.mp3", "51.mp3", "52.mp3", "53.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaF: 21, // Start displaying from episode 21
+  dramaF: 30, // Start displaying from episode 30
   song1: 1, // Start displaying from episode 1
 };
 
