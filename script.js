17df099082ace9a618a50ce532f5f0c375c608f2
doveletchan3 <doveletchan3@gmail.com>
2025-03-05 00:18:49 -0800

    天外金球 眼睛

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const episodes = {
-  dramaF: ["79.mp3", "80.mp3", "81.mp3", "82.mp3","83.mp3", "84.mp3", "85.mp3"],
-  dramaJ: ["01.mp3", "02.mp3","03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3","23.mp3"],   
-  dramaK: ["03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3"],  
+  dramaJ: ["23.mp3"],   
+  dramaL: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
+  dramaM: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaF: 79, // Start displaying from episode 79
-  dramaJ: 1, // Start displaying from episode 1
-  dramaK: 3, // Start displaying from episode 3
+  dramaJ: 23, // Start displaying from episode 23
+  dramaL: 1, // Start displaying from episode 1
+  dramaM: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaF: 1, // Increment by 1
   dramaJ: 1, // Increment by 1
-  dramaK: 1, // Increment by 1
+  dramaL: 1, // Increment by 1
+  dramaM: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
