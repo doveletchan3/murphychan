3006697db5a7fb4a7a604b06f340765aa0e384a5
doveletchan3 <doveletchan3@gmail.com>
2025-03-16 00:13:28 -0700

    蜂雲

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const episodes = {
   dramaI: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
+  dramaJ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
   dramaI: 1, // Start displaying from episode 1
+  dramaJ: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
   dramaI: 1, // Increment by 1
+  dramaJ: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
