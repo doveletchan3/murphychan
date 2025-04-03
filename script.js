792251d01fc55bd7b15201459c6120cf74af0808
doveletchan3 <doveletchan3@gmail.com>
2025-04-02 21:05:45 -0700

    Delete

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const episodes = {
-  dramaE: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"], 
   dramaF: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   dramaG: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
@@ -7,7 +6,6 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaE: 1, // Start displaying from episode 1
   dramaF: 1, // Start displaying from episode 1
   dramaG: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
@@ -15,7 +13,6 @@ const startingEpisodes = {
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaE: 1, // Increment by 1   
   dramaF: 1, // Increment by 1 
   dramaG: 1, // Increment by 1 
   song1: 1, // Increment by 1
