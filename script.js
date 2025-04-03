e72953276604123aa787c76691ab6c84866f47b8
doveletchan3 <doveletchan3@gmail.com>
2025-04-02 22:18:48 -0700

    犀照

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const episodes = {
   dramaF: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   dramaG: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
+  dramaH: ["01.mp3", "02.mp3", "03.mp3", "04.mp3","05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3","15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 };
 
@@ -8,6 +9,7 @@ const episodes = {
 const startingEpisodes = {
   dramaF: 1, // Start displaying from episode 1
   dramaG: 1, // Start displaying from episode 1
+  dramaH: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
@@ -15,6 +17,7 @@ const startingEpisodes = {
 const episodeIncrements = {
   dramaF: 1, // Increment by 1 
   dramaG: 1, // Increment by 1 
+  dramaH: 1, // Increment by 1 
   song1: 1, // Increment by 1
 };
 
