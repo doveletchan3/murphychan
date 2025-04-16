5e5806686895d4f81768856dc48aaa915462a9bc
doveletchan3 <doveletchan3@gmail.com>
2025-04-15 23:36:52 -0700

    三毛流浪記

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const episodes = {
-  dramaO: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3"],
-  dramaP: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
+  dramaP: ["08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3", "35.mp3"],
   dramaQ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3"],
+  dramaR: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3"],
   song1: ["song1.mp3", "song2.mp3"],  
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaO: 1, // Start displaying from episode 1
-  dramaP: 1, // Start displaying from episode 1 
+  dramaP: 8, // Start displaying from episode 8
   dramaQ: 1, // Start displaying from episode 1 
+  dramaR: 1, // Start displaying from episode 1 
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaO: 1, // Increment by 1
   dramaP: 1, // Increment by 1
   dramaQ: 1, // Increment by 1
+  dramaR: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
