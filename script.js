e217a99972464f928ef891c522ad9e16fc94f547
doveletchan3 <doveletchan3@gmail.com>
2025-04-26 20:44:13 -0700

    白蛇傳 (33-56)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const episodes = {
   dramaX: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   dramaZ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
-  dramaA: ["32.mp3", "33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3"],
+  dramaA: ["33.mp3", "34.mp3", "35.mp3", "36.mp3", "37.mp3", "38.mp3", "39.mp3", "40.mp3","41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3", "49.mp3", "50.mp3", "51.mp3", "52.mp3", "53.mp3", "54.mp3", "55.mp3", "56.mp3"],
   dramaB: ["01.mp3", "02.mp3", "03.mp3", "04.mp3"],
   song1: ["song1.mp3", "song2.mp3"],  
 };
@@ -10,7 +10,7 @@ const episodes = {
 const startingEpisodes = {
   dramaX: 1, // Start displaying from episode 1
   dramaZ: 1, // Start displaying from episode 1
-  dramaA: 32, // Start displaying from episode 32
+  dramaA: 33, // Start displaying from episode 33
   dramaB: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
