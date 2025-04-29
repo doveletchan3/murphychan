25594cf2feaf0458989f0e3393e7b9c8fb975ca9
doveletchan3 <doveletchan3@gmail.com>
2025-04-29 00:14:15 -0700

    尋秦記(1-24)

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 const episodes = {
-  dramaX: ["10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
   dramaZ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
-  dramaA: ["55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3", "61.mp3", "62.mp3", "63.mp3", "64.mp3"],
-  dramaB: ["01.mp3", "02.mp3", "03.mp3", "04.mp3"],
   dramaC: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"],  
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaX: 10, // Start displaying from episode 10
   dramaZ: 1, // Start displaying from episode 1
-  dramaA: 55, // Start displaying from episode 55
-  dramaB: 1, // Start displaying from episode 1
   dramaC: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaX: 1, // Increment by 1
   dramaZ: 1, // Increment by 1
-  dramaA: 1, // Increment by 1
-  dramaB: 1, // Increment by 1
   dramaC: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
