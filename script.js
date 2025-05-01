84407d7dee202a2e6d12eb626f90de1c32100349
doveletchan3 <doveletchan3@gmail.com>
2025-04-30 22:15:16 -0700

    楚留香 (1-24)

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const episodes = {
   dramaC: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
+  dramaD: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"],  
 };
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaZ: 1, // Start displaying from episode 1
   dramaC: 1, // Start displaying from episode 1
+  dramaD: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaZ: 1, // Increment by 1
   dramaC: 1, // Increment by 1
+  dramaD: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
