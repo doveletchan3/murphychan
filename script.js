a9824f2270d95e81e8d02d22ffea01a884fbfc01
doveletchan3 <doveletchan3@gmail.com>
2025-05-02 22:22:12 -0700

    福爾摩斯探案 (1-24)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const episodes = {
   dramaC: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   dramaD: ["17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3"],
   dramaE: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
+  dramaF: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"],  
 };
 
@@ -10,6 +11,7 @@ const startingEpisodes = {
   dramaC: 1, // Start displaying from episode 1
   dramaD: 17, // Start displaying from episode 17
   dramaE: 1, // Start displaying from episode 1
+  dramaF: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
@@ -18,6 +20,7 @@ const episodeIncrements = {
   dramaC: 1, // Increment by 1
   dramaD: 1, // Increment by 1
   dramaE: 1, // Increment by 1
+  dramaF: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
