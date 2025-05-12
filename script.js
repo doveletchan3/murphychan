2f85d459a93316f0dc06231d56d959a226e61f51
doveletchan3 <doveletchan3@gmail.com>
2025-05-12 00:19:03 -0700

    Del

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const episodes = {
-  dramaG: ["122.mp3", "123.mp3", "124.mp3", "125.mp3", "126.mp3", "127.mp3", "128.mp3", "129.mp3", "130.mp3", "131.mp3", "132.mp3", "133.mp3", "134.mp3", "135.mp3", "136.mp3", "137.mp3", "138.mp3"],
-  dramaH: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
-  dramaI: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3"],
+  dramaH: ["11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3", "33.mp3", "34.mp3"],
   dramaJ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
   song1: ["song1.mp3", "song2.mp3"], 
 //
@@ -11,18 +9,14 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaG: 122, // Start displaying from episode 122
   dramaH: 1, // Start displaying from episode 1  
-  dramaI: 1, // Start displaying from episode 1 
   dramaJ: 1, // Start displaying from episode 1 
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaG: 1, // Increment by 1
   dramaH: 1, // Increment by 1
-  dramaI: 1, // Increment by 1
   dramaJ: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
