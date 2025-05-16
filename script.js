8b94443ee2d9de235bb259eb28aa62abce5964a0
doveletchan3 <doveletchan3@gmail.com>
2025-05-15 20:07:19 -0700

    小李飛刀 (67-90)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const episodes = {
-  dramaH: ["58.mp3", "59.mp3", "60.mp3", "61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3", "76.mp3", "77.mp3", "78.mp3", "79.mp3", "80.mp3", "81.mp3"],
+  dramaH: ["67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3", "76.mp3", "77.mp3", "78.mp3", "79.mp3", "80.mp3", "81.mp3", "82.mp3", "83.mp3", "84.mp3", "85.mp3", "86.mp3", "87.mp3", "88.mp3", "89.mp3", "90.mp3"],
   dramaJ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
   dramaK: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"], 
@@ -10,7 +10,7 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaH: 58, // Start displaying from episode 58
+  dramaH: 67, // Start displaying from episode 67
   dramaJ: 1, // Start displaying from episode 1 
   dramaK: 1, // Start displaying from episode 1 
   song1: 1, // Start displaying from episode 1
