c864ffd1330867b7ea2d0c19dd3c76dda8404eda
doveletchan3 <doveletchan3@gmail.com>
2025-06-09 22:16:33 -0700

    我們不是天使 (1-12)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const episodes = {
   dramaS: ["61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3"],
   dramaT: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   dramaU: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
+  dramaU1: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3"],
   dramaV: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   dramaW: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   dramaX: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
@@ -19,6 +20,7 @@ const startingEpisodes = {
   dramaS: 61, // Start displaying from episode 61
   dramaT: 1, // Start displaying from episode 1
   dramaU: 1, // Start displaying from episode 1
+  dramaU1: 1, // Start displaying from episode 1
   dramaV: 1, // Start displaying from episode 1
   dramaW: 1, // Start displaying from episode 1
   dramaX: 1, // Start displaying from episode 1
@@ -32,6 +34,7 @@ const episodeIncrements = {
   dramaS: 1, // Increment by 1
   dramaT: 1, // Increment by 1
   dramaU: 1, // Increment by 1
+  dramaU1: 1, // Increment by 1
   dramaV: 1, // Increment by 1
   dramaW: 1, // Increment by 1  
   dramaX: 1, // Increment by 1 
