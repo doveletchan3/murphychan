3af5c8913941d0f56fa8239c4988d697f3dd9946
doveletchan3 <doveletchan3@gmail.com>
2025-07-18 00:06:11 -0700

    心扉的信 1-10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const episodes = {
   dramaA: ["37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3", "49.mp3", "50.mp3", "51.mp3", "52.mp3", "53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3"],
-  dramaC: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
-  dramaE: ["17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3", "26.mp3", "27.mp3", "28.mp3", "29.mp3", "30.mp3", "31.mp3", "32.mp3"],
+  dramaD: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
+  dramaF: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
   dramaQ: ["53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3", "61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3", "76.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 //
@@ -12,17 +12,17 @@ const episodes = {
 // Define starting episodes for each drama
 const startingEpisodes = {
   dramaA: 37, // Start displaying from episode 1
-  dramaC: 1, // Start displaying from episode 1
-  dramaE: 17, // Start displaying from episode 1
+  dramaD: 1, // Start displaying from episode 1
+  dramaF: 1, // Start displaying from episode 1
   dramaQ: 53, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaA: 1, // Increment by 1 
-  dramaC: 1, // Increment by 1
-  dramaE: 1, // Increment by 1  
+  dramaA: 1, // Increment by 1
+  dramaD: 1, // Increment by 1  
+  dramaF: 1, // Increment by 1
   dramaQ: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
