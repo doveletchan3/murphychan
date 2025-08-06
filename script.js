1e168983d125ceb1ee337550fa846fc9ba03c88a
doveletchan3 <doveletchan3@gmail.com>
2025-08-05 23:36:10 -0700

    施公案 97

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const episodes = {
   dramaA: ["37.mp3", "38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3", "49.mp3", "50.mp3", "51.mp3", "52.mp3", "53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3"],
-  dramaP: ["91.mp3", "92.mp3", "93.mp3", "94.mp3", "95.mp3", "96.mp3", "97.mp3", "98.mp3", "99.mp3", "100.mp3", "101.mp3", "102.mp3", "103.mp3", "104.mp3", "105.mp3", "106.mp3", "107.mp3", "108.mp3", "109.mp3", "110.mp3"],
+  dramaP: ["97.mp3", "98.mp3", "99.mp3", "100.mp3", "101.mp3", "102.mp3", "103.mp3", "104.mp3", "105.mp3", "106.mp3", "107.mp3", "108.mp3", "109.mp3", "110.mp3", "111.mp3", "112.mp3", "113.mp3", "114.mp3", "115.mp3", "116.mp3"],
   dramaQ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   dramaR: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
@@ -12,7 +12,7 @@ const episodes = {
 // Define starting episodes for each drama
 const startingEpisodes = {
   dramaA: 37, // Start displaying from episode 1
-  dramaP: 91, // Start displaying from episode 1
+  dramaP: 97, // Start displaying from episode 1
   dramaQ: 1, // Start displaying from episode 1
   dramaR: 1, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
