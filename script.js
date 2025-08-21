9777db75e095d08961750a3d2e401fb5e8abd723
doveletchan3 <doveletchan3@gmail.com>
2025-08-21 00:03:18 -0700

    鬱金香 1-20

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const episodes = {
-  dramaX: ["92.mp3", "93.mp3", "94.mp3", "95.mp3", "96.mp3", "97.mp3", "98.mp3", "99.mp3", "100.mp3", "101.mp3", "102.mp3", "103.mp3"],
   dramaY: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
   dramaZ: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3"],
   dramaA: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3"],
@@ -11,7 +10,6 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaX: 92, // Start displaying from episode 1
   dramaY: 1, // Start displaying from episode 1
   dramaZ: 1, // Start displaying from episode 1
   dramaA: 1, // Start displaying from episode 1
@@ -20,7 +18,6 @@ const startingEpisodes = {
 
 // Define the increment value for each drama
 const episodeIncrements = {
-  dramaX: 1, // Increment by 1
   dramaY: 1, // Increment by 1
   dramaZ: 1, // Increment by 1
   dramaA: 1, // Increment by 1
