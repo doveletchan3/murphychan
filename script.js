ed53b3b5ca52f38aab03660265664a7b762bf6a6
doveletchan3 <doveletchan3@gmail.com>
2025-10-14 00:16:09 -0700

    誅仙 64-87

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const episodes = {
-  dramaW: ["53.mp3", "54.mp3", "55.mp3", "56.mp3", "57.mp3", "58.mp3", "59.mp3", "60.mp3", "61.mp3", "62.mp3", "63.mp3", "64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3", "76.mp3"],
+  dramaW: ["64.mp3", "65.mp3", "66.mp3", "67.mp3", "68.mp3", "69.mp3", "70.mp3", "71.mp3", "72.mp3", "73.mp3", "74.mp3", "75.mp3", "76.mp3", "77.mp3", "78.mp3", "79.mp3", "80.mp3", "81.mp3", "82.mp3", "83.mp3", "84.mp3", "85.mp3", "86.mp3", "87.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 //
 
@@ -8,7 +8,7 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaW: 53, // Start displaying from episode 1
+  dramaW: 64, // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
