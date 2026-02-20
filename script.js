0356fda4eefcf8315f183b23cfc3d99472163c36
doveletchan3 <doveletchan3@gmail.com>
2026-02-20 00:12:03 -0800

    倚天屠龍記 100-104

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const episodes = {
-  dramaQ: ["78.mp3", "79.mp3", "80.mp3", "81.mp3", "82.mp3", "83.mp3", "84.mp3", "85.mp3", "86.mp3", "87.mp3", "88.mp3", "89.mp3", "90.mp3", "91.mp3", "92.mp3", "93.mp3", "94.mp3", "95.mp3", "96.mp3", "97.mp3", "98.mp3", "99.mp3", "100.mp3"],
+  dramaQ: ["100.mp3", "101.mp3", "102.mp3", "103.mp3", "104.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 //
 //  dramaXXX: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
@@ -8,7 +8,7 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaQ: 78, // Start displaying from episode 78
+  dramaQ: 100, // Start displaying from episode 100
   song1: 1, // Start displaying from episode 1
 };
 
