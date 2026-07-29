bf7ddb2c9558b2d0e490560366b53cc045c3c4b2
doveletchan3 <doveletchan3@gmail.com>
2026-07-29 00:41:27 -0700

    古代民間故事劇 395-412

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const episodes = {
-  dramaH: ["379.mp3", "380.mp3", "381.mp3", "382.mp3", "383.mp3", "384.mp3", "385.mp3", "386.mp3", "387.mp3", "388.mp3", "389.mp3", "390.mp3", "391.mp3", "392.mp3", "393.mp3", "394.mp3", "395.mp3", "396.mp3"],
+  dramaH: ["395.mp3", "396.mp3", "397.mp3", "398.mp3", "399.mp3", "400.mp3", "401.mp3", "402.mp3", "403.mp3", "404.mp3", "405.mp3", "406.mp3", "407.mp3", "408.mp3", "409.mp3", "410.mp3", "411.mp3", "412.mp3"],
   song1: ["song1.mp3", "song2.mp3"],
 //
 //  dramaXXX: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
@@ -8,7 +8,7 @@ const episodes = {
 
 // Define starting episodes for each drama
 const startingEpisodes = {
-  dramaH: 379,  // Start displaying from episode 1
+  dramaH: 395,  // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
