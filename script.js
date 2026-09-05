21b9212111cc84bd14c7565892e35ffe6992cae2
doveletchan3 <doveletchan3@gmail.com>
2026-09-04 21:44:10 -0700

    日與夜 1-25

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const episodes = {
   dramaK: ["38.mp3", "39.mp3", "40.mp3", "41.mp3", "42.mp3", "43.mp3", "44.mp3", "45.mp3", "46.mp3", "47.mp3", "48.mp3"],
   dramaL: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3"],
   dramaM: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3"],
+  dramaN: ["01.mp3", "02.mp3", "03.mp3", "04.mp3", "05.mp3", "06.mp3", "07.mp3", "08.mp3", "09.mp3", "10.mp3", "11.mp3", "12.mp3", "13.mp3", "14.mp3", "15.mp3", "16.mp3", "17.mp3", "18.mp3", "19.mp3", "20.mp3", "21.mp3", "22.mp3", "23.mp3", "24.mp3", "25.mp3"],
   song1: ["01.mp3","02.mp3"],
   
 //
@@ -14,6 +15,7 @@ const startingEpisodes = {
   dramaK: 38,  // Start displaying from episode 1
   dramaL: 1,  // Start displaying from episode 1
   dramaM: 1,  // Start displaying from episode 1
+  dramaN: 1,  // Start displaying from episode 1
   song1: 1, // Start displaying from episode 1
 };
 
@@ -22,6 +24,7 @@ const episodeIncrements = {
   dramaK: 1, // Increment by 1
   dramaL: 1, // Increment by 1
   dramaM: 1, // Increment by 1
+  dramaN: 1, // Increment by 1
   song1: 1, // Increment by 1
 };
 
